660556d00dae16755dce9a5a4c57a5927a51470b
ZobovaA <75171940+ZobovaA@users.noreply.github.com>
2021-11-03 20:36:16 +0200

    test

--- tests/pageobjects/medication.page.ts
+++ tests/pageobjects/medication.page.ts
@@ -1,82 +1,82 @@
-import { Page } from "./page";
-class MedicationPage extends Page {
-    get NewRqstbtn() {
-        return $(".btn.btn-primary");
-    }
+// import { Page } from "./page";
+// class MedicationPage extends Page {
+//     get NewRqstbtn() {
+//         return $(".btn.btn-primary");
+//     }
 
-    get currentTitle() {
-        return $(".view-current-title");
-    }
+//     get currentTitle() {
+//         return $(".view-current-title");
+//     }
 
-    get MedicationHeader() {
-        return $(".view-current-title");
-    }
+//     get MedicationHeader() {
+//         return $(".view-current-title");
+//     }
 
-    get MedicationSubLinks() {
-        return $$(".category-sub-item");
-    }
+//     get MedicationSubLinks() {
+//         return $$(".category-sub-item");
+//     }
 
-    get MedicationLink() {
-        return $("#ember767");
-    }
+//     get MedicationLink() {
+//         return $("#ember767");
+//     }
 
-    get visitDrop() {
-        return $(".test-add-visit select");
-    }
+//     get visitDrop() {
+//         return $(".test-add-visit select");
+//     }
 
-    get inputPatient() {
-        return $(".test-patient-input .tt-input");
-    }
+//     get inputPatient() {
+//         return $(".test-patient-input .tt-input");
+//     }
 
-    get visitDropOption() {
-        return $("option=8/11/2021 (Pharmacy)");
-    }
+//     get visitDropOption() {
+//         return $("option=8/11/2021 (Pharmacy)");
+//     }
 
-    get inputMedication() {
-        return $(".test-medication-input .tt-input");
-    }
+//     get inputMedication() {
+//         return $(".test-medication-input .tt-input");
+//     }
 
-    get inputMedicationSug() {
-        return $(".test-medication-input .tt-suggestion");
-    }
+//     get inputMedicationSug() {
+//         return $(".test-medication-input .tt-suggestion");
+//     }
 
-    get inputPrescription() {
-        return $(".test-medication-prescription textarea");
-    }
+//     get inputPrescription() {
+//         return $(".test-medication-prescription textarea");
+//     }
 
-    get dropMedication() {
-        return $(".test-add-visit .form-control");
-    }
+//     get dropMedication() {
+//         return $(".test-add-visit .form-control");
+//     }
 
-    get inputDate() {
-        return $(".col-sm-4.col-sm-4 input");
-    }
+//     get inputDate() {
+//         return $(".col-sm-4.col-sm-4 input");
+//     }
 
-    get refilsInput() {
-        return $("input[id^=refills]");
-    }
+//     get refilsInput() {
+//         return $("input[id^=refills]");
+//     }
 
-    get inputQuantity() {
-        return $(".test-quantity-input .ember-text-field");
-    }
+//     get inputQuantity() {
+//         return $(".test-quantity-input .ember-text-field");
+//     }
 
-    get MedicationAddBtn() {
-        return $(".panel-footer .btn-primary");
-    }
+//     get MedicationAddBtn() {
+//         return $(".panel-footer .btn-primary");
+//     }
 
-    get CalendarInput() {
-        return $(".col-sm-4 .ember-text-field");
-    }
+//     get CalendarInput() {
+//         return $(".col-sm-4 .ember-text-field");
+//     }
 
-    get btnOk() {
-        return $("button=Ok");
-    }
+//     get btnOk() {
+//         return $("button=Ok");
+//     }
 
     
 
-    open() {
-        return super.open("medication");
-    }
-}
+//     open() {
+//         return super.open("medication");
+//     }
+// }
 
-export const medicationPage = new MedicationPage();
+// export const medicationPage = new MedicationPage();
--- tests/specs/madication.spec.ts
+++ tests/specs/madication.spec.ts
@@ -15,49 +15,49 @@ describe("Request a new medication ", () => {
         await loginPage.loginFn();
     });
 
-    it("clicked to medication section", async () => {
-        await medicationPage.MedicationLink.click();
-        await expect(medicationPage.MedicationSubLinks).toBeElementsArrayOfSize(
-            4
-        );
-        await expect(medicationPage.MedicationSubLinks[0]).toHaveTextContaining(
-            "Requests"
-        );
-        await expect(medicationPage.MedicationSubLinks[1]).toHaveTextContaining(
-            "Completed"
-        );
-        await expect(medicationPage.MedicationSubLinks[2]).toHaveTextContaining(
-            "New Request"
-        );
-        await expect(medicationPage.MedicationSubLinks[3]).toHaveTextContaining(
-            "Return Medication"
-        );
+//     it("clicked to medication section", async () => {
+//         await medicationPage.MedicationLink.click();
+//         await expect(medicationPage.MedicationSubLinks).toBeElementsArrayOfSize(
+//             4
+//         );
+//         await expect(medicationPage.MedicationSubLinks[0]).toHaveTextContaining(
+//             "Requests"
+//         );
+//         await expect(medicationPage.MedicationSubLinks[1]).toHaveTextContaining(
+//             "Completed"
+//         );
+//         await expect(medicationPage.MedicationSubLinks[2]).toHaveTextContaining(
+//             "New Request"
+//         );
+//         await expect(medicationPage.MedicationSubLinks[3]).toHaveTextContaining(
+//             "Return Medication"
+//         );
 
-        await medicationPage.NewRqstbtn.click();
+//         await medicationPage.NewRqstbtn.click();
 
-        await expect(medicationPage.currentTitle).toHaveTextContaining(
-            "New Medication Request"
-        );
+//         await expect(medicationPage.currentTitle).toHaveTextContaining(
+//             "New Medication Request"
+//         );
 
-        await browser.pause(4000);
-        await medicationPage.inputPatient.setValue("Test - Patient - P00201");
-        await medicationPage.inputPatient.click(); 
+//         await browser.pause(4000);
+//         await medicationPage.inputPatient.setValue("Test - Patient - P00201");
+//         await medicationPage.inputPatient.click(); 
 
-        await medicationPage.visitDrop.click();
-        await medicationPage.visitDropOption.click(); 
-        await medicationPage.inputMedication.setValue("Pramoxine"); 
-        await medicationPage.inputMedicationSug.click();
+//         await medicationPage.visitDrop.click();
+//         await medicationPage.visitDropOption.click(); 
+//         await medicationPage.inputMedication.setValue("Pramoxine"); 
+//         await medicationPage.inputMedicationSug.click();
 
-        await medicationPage.inputPrescription.setValue("Testing prescription");
+//         await medicationPage.inputPrescription.setValue("Testing prescription");
 
-        await medicationPage.inputDate.setValue("11/3/2021"); 
+//         await medicationPage.inputDate.setValue("11/3/2021"); 
 
-        await medicationPage.inputPrescription.click();
-        await medicationPage.inputPrescription.click();
-        await medicationPage.inputQuantity.setValue(getRandom(1, 5));
+//         await medicationPage.inputPrescription.click();
+//         await medicationPage.inputPrescription.click();
+//         await medicationPage.inputQuantity.setValue(getRandom(1, 5));
 
-        await  medicationPage.refilsInput.setValue(getRandom(5, 10)); 
+//         await  medicationPage.refilsInput.setValue(getRandom(5, 10)); 
 
-        await medicationPage.btnOk.click();
-    });
-});
+//         await medicationPage.btnOk.click();
+//     });
+// });
